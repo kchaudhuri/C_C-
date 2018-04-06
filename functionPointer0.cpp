b42c78bddd8f639e421718d1eba2900f35e09969
DESKTOP-2CHP3MC\Kaustubh Chaudhuri <kaustubhc2014@gmail.com>
2018-04-05 17:43:16 -0700

    Practice basic function pointer.

---
+++ functionPointer0.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+using namespace std;
+
+void func()
+{
+    printf("in func\n");
+}
+
+int main()
+{
+    printf("in main\n");
+
+    void (*fp)() = func;
+    fp();
+
+    return 0;
+}
